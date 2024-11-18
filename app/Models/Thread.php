8a981eeed02c7d08f28b6f7ae3f5e9badf799019
Bruno Lima <bru.no@outlook.com.br>
2024-11-18 17:14:10 -0300

    Adiciona lógica para geração automática do slug

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -9,7 +9,26 @@ class Thread extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'body'];
+    protected $fillable = ['title', 'body', 'slug'];
+
+    protected static function boot()
+{
+    parent::boot();
+
+    // Evento ao criar uma nova thread
+    static::creating(function ($thread) {
+        if (empty($thread->slug)) {
+            $thread->slug = \Illuminate\Support\Str::slug($thread->title);
+        }
+    });
+
+    // Evento ao atualizar uma thread
+    static::updating(function ($thread) {
+        if ($thread->isDirty('title')) {
+            $thread->slug = \Illuminate\Support\Str::slug($thread->title);
+        }
+    });
+}
 
     public function user()
     {
