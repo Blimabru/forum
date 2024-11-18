578be0496df2896faa3a5d33b6e5f0773af15934
Bruno Lima <bru.no@outlook.com.br>
2024-11-18 17:15:41 -0300

    Adiciona rotas para criação, exibição e edição de tópicos e respostas

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,30 @@
 <?php
 
+use App\Http\Controllers\ReplyController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\ThreadController;
 
-Route::get('/', function() {
+Route::get('/', function () {
     return Inertia::render('Welcome');
 });
 
-Route::resource('threads', ThreadController::class);
+Route::post('/threads/replies', [ReplyController::class, 'store'])->name('replies.store');
+
+Route::resource('threads', ThreadController::class)->except(['create', 'show', 'edit', 'destroy']);
+
+
+Route::get('/threads/create', [ThreadController::class, 'create'])->name('threads.create');
+
+
+Route::get('/threads/{slug}', [ThreadController::class, 'show'])->name('threads.show');
+
+
+Route::get('/threads/edit/{slug}', [ThreadController::class, 'edit'])->name('threads.edit');
+
+
+Route::delete('/threads/destroy/{slug}', [ThreadController::class, 'destroy'])->name('threads.destroy');
+
 
 
 
