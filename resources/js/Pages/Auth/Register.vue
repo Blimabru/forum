cb95a4e10b24553e3bd0f48433e6e1cf9828b9b7
Bruno Lima <bru.no@outlook.com.br>
2024-11-18 17:16:44 -0300

    Importação do componente Header

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -7,6 +7,12 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
+import Header from '@/Components/Header.vue';
+
+components:
+    {
+        Header
+    }
 
 const form = useForm({
     name: '',
@@ -24,6 +30,7 @@ const submit = () => {
 </script>
 
 <template>
+    <Header />
     <Head title="Register" />
 
     <AuthenticationCard>
@@ -33,7 +40,7 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="name" value="Name" />
+                <InputLabel for="name" value="Nome:" />
                 <TextInput
                     id="name"
                     v-model="form.name"
@@ -47,7 +54,7 @@ const submit = () => {
             </div>
 
             <div class="mt-4">
-                <InputLabel for="email" value="Email" />
+                <InputLabel for="email" value="E-mail:" />
                 <TextInput
                     id="email"
                     v-model="form.email"
@@ -60,7 +67,7 @@ const submit = () => {
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+                <InputLabel for="password" value="Senha:" />
                 <TextInput
                     id="password"
                     v-model="form.password"
@@ -73,7 +80,7 @@ const submit = () => {
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password_confirmation" value="Confirm Password" />
+                <InputLabel for="password_confirmation" value="Confirme a senha:" />
                 <TextInput
                     id="password_confirmation"
                     v-model="form.password_confirmation"
@@ -100,11 +107,11 @@ const submit = () => {
 
             <div class="flex items-center justify-end mt-4">
                 <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Already registered?
+                    Já possui cadastro?
                 </Link>
 
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
+                    Cadastrar
                 </PrimaryButton>
             </div>
         </form>
