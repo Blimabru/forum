d388eac180d87f212fa4d4631a9bc9d70b7cde43
Bruno Lima <bru.no@outlook.com.br>
2024-11-18 17:12:34 -0300

    Adiciona funcionalidades de listagem, criação, edição, atualização e exclusão de tópicos.

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Thread;
 use Inertia\Inertia;
+use App\Http\Requests\ValidatorForm;
+use App\Models\{Thread, User};
+use Illuminate\Support\Str;
 
 class ThreadController extends Controller
 {
@@ -17,16 +18,20 @@ class ThreadController extends Controller
     /**
      * Display a listing of the resource.
      */
+
     public function index()
     {
-        $threads = Thread::all();
+        $threads = Thread::orderBy('created_at', 'desc')->paginate(15);
 
         return Inertia::render('Threads/Index', [
-            'threads' => $threads
+            'threads' => $threads->items(),
+            'pagination' => [
+                'current_page' => $threads->currentPage(),
+                'last_page' => $threads->lastPage(),
+                'total' => $threads->total(),
+            ],
+            'flash' => session()->all()
         ]);
-
-        //$threads = $this->thread->paginate(15);
-        //return view('thread.index', compact('threads'));
     }
 
     /**
@@ -35,109 +40,111 @@ class ThreadController extends Controller
     public function create()
     {
         return Inertia::render('Threads/Create');
-
-        //return view('thread.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ValidatorForm $validatorForm)
     {
-        // Validação
-        $request->validate([
-            'title' => 'required|string|max:255',
-            'body' => 'required|string',
-        ]);
-
-        // Criar novo thread
-        Thread::create($request->only('title', 'body'));
-
-        // Redirecionar de volta com uma mensagem de sucesso
+        $threadData = $validatorForm->validated();
+    
+        $user = User::find(1);
+        $user->threads()->create($threadData);
+    
         return redirect()->route('threads.index')
-                         ->with('success', 'Thread criado com sucesso!');
-
-        // try {
-        //     $this->thread->create($request->all());
-
-        //     dd('Tópico criado com sucesso!');
-        // } catch (\Exception $e) {
-
-        //     dd($e->getMessage());
-        // }
+            ->with('flash', [
+                'message' => 'Tópico criado com sucesso!',
+                'type' => 'success',
+            ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Thread $thread /*string $id*/)
-    {
-        return Inertia::render('Threads/Show', [
-            'thread' => $thread
-        ]);
 
-        //return redirect()->route('threads.edit', $id);
+    public function show(string $slug)
+    {
+        $thread = Thread::where('slug', $slug)
+            ->with('replies.user')
+            ->first();
+
+        $user = $thread->user;
+
+        return inertia::render('Threads/Show', [
+            'thread' => $thread,
+            'user' => $user,
+            'replies' => $thread->replies,
+        ]);
     }
+
+    // public function show(string $slug)
+    // {
+    //     $thread = Thread::where('slug', $slug)->firstOrFail();
+    //     $user = $thread->user;
+
+    //     return Inertia::render('Threads/Show', [
+    //         'thread' => $thread,
+    //         'user' => $user
+    //     ]);
+    // }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Thread $thread /*string $id*/)
+    public function edit(string $slug)
     {
+        $thread = Thread::where('slug', $slug)->firstOrFail();
+
         return Inertia::render('Threads/Edit', [
             'thread' => $thread
         ]);
-
-        //$this->thread->find($id);
-        //return view('thread.edit', compact('threads'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Thread $thread /*string $id*/)
+    public function update(ValidatorForm $validatorForm, Thread $thread)
     {
-        $request->validate([
-            'title' => 'required|string|max:255',
-            'body' => 'required|string',
+        $thread->update([
+            'title' => $validatorForm->title,
+            'body' => $validatorForm->body,
         ]);
 
-        // Atualizar o thread
-        $thread->update($request->only('title', 'body'));
-
         return redirect()->route('threads.index')
-                         ->with('success', 'Thread atualizado com sucesso!');
-
-        // try {
-        //     $thread = $this->thread->find($id);
-        //     $thread->update($request->all());
-
-        //     dd('Tópico atualizado com sucesso!');
-        // } catch (\Exception $e) {
-
-        //     dd($e->getMessage());
-        // }
+            ->with('flash', [
+                'message' => 'Tópico atualizado!',
+                'type' => 'success',
+            ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Thread $thread /*string $id*/)
+    // public function destroy(string $slug)
+    // {
+    //     $thread = Thread::where('slug', $slug)->first();
+
+    //     // Verifique se o tópico existe
+    //     if ($thread) {
+    //         $thread->delete();
+    //         return response()->json(['message' => 'Tópico excluído com sucesso!'], 200);
+    //     }
+
+    //     // Se não encontrar o tópico
+    //     return response()->json(['error' => 'Erro ao tentar excluir o tópico. Tente novamente.'], 400);
+    // }
+
+    public function destroy(string $slug)
     {
-        // Deletar o thread
+        $thread = Thread::where('slug', $slug)->firstOrFail();
+
         $thread->delete();
 
         return redirect()->route('threads.index')
-                         ->with('success', 'Thread excluído com sucesso!');
-    
-        // try {
-        //     $thread = $this->thread->find($id);
-        //     $thread->delete();
-
-        //     dd('Tópico removido com sucesso!');
-        // } catch (\Exception $e) {
-
-        //     dd($e->getMessage());
-        // }
+            ->with('flash', [
+                'message' => 'Tópico excluído com sucesso!',
+                'type' => 'danger',
+            ]);
     }
 }
